283dfa65b73b71f00fef98fdd459115d2f11c5eb
Luu Dinh Mac <nguoiemcuanui@gmail.com>
2016-12-13 21:15:13 +0700

    xoa

--- Website/WebContent/LibraryPL/Scripts/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("opentFile").addEventListener('change',function(){
-	var fr = new FileReader();
-		fr.onload = function(){
-		document.getElementById("fileContetnts").textContent = this.result;
-		}
-		fr.readAsText(this.files[0]);
-})
